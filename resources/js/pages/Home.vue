9f52ae56b55a83603ba13f7939056b63a1d3a1ae
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-02 22:40:32 +0200

    chore: init

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import '../../css/app.css';
+</script>
+<template>
+<div>
+    <h1 class="bg-red-300 text-center">Hello</h1>
+    <button>Estudante</button>
+    <button>Docente</button>
+    <button>Funcionário</button>
+
+</div>
+<section>
+<div>Cadeiras Gerais</div>
+<div>Registo Academico</div>
+<div>Eng. Quimica</div>
+<div>Electrotecnica</div>
+<div>Mecanica</div>
+<div>Geologia e Minas</div>
+</section>
+</template>
