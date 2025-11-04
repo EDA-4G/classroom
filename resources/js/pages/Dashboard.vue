68aa2379c9714dd694f9a782823897172e5a4925
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-05 01:50:49 +0200

    refactor(dot navigation): add Dashboard component

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -13,11 +13,12 @@ import {
     CarouselPrevious,
 } from '@/components/ui/carousel'
 
-import { ChevronLeft, ChevronRight, FolderOpen } from 'lucide-vue-next';
+import { ChevronLeft, ChevronRight, FolderOpen, Search } from 'lucide-vue-next';
 
 import emblaCarouselVue from 'embla-carousel-vue'
 import Autoplay from 'embla-carousel-autoplay'
-import { ref } from 'vue';
+// import { ref } from 'vue';
+// import Input from '@/components/ui/input/Input.vue';
 
 const [emblaRef, emblaApi] = emblaCarouselVue({ loop: true }, [Autoplay({ delay: 8000 })])
 function scrollNext() {
@@ -28,16 +29,57 @@ function scrollPrev() {
     emblaApi.value?.scrollPrev()
 }
 
-const selectedIndex = ref(0)
-// 🖼️ Slides
+
+
+
+
+
+
+import { ref, onMounted, onUnmounted } from 'vue'
+// import emblaCarouselVue from 'embla-carousel-vue'
+// import Autoplay from 'embla-carousel-autoplay'
+
 const slides = [
-    'https://picsum.photos/id/1005/800/400',
     'https://picsum.photos/id/1015/800/400',
-    'https://picsum.photos/id/1020/800/400',
-    'https://picsum.photos/id/1024/800/400',
-    'https://picsum.photos/id/1035/800/400',
-    'https://picsum.photos/id/1045/800/400'
+    'https://picsum.photos/id/1016/800/400',
+    'https://picsum.photos/id/1018/800/400',
 ]
+
+// const [emblaRef, emblaApi] = emblaCarouselVue(
+//   { loop: true, align: 'center' },
+//   [Autoplay({ delay: 8000 })]
+// )
+
+const selectedIndex = ref(0)
+
+// function scrollNext() {
+//   emblaApi.value?.scrollNext()
+// }
+
+// function scrollPrev() {
+//   emblaApi.value?.scrollPrev()
+// }
+
+function scrollTo(index: number) {
+    emblaApi.value?.scrollTo(index)
+}
+
+function updateSelected() {
+    if (!emblaApi.value) return
+    selectedIndex.value = emblaApi.value.selectedScrollSnap()
+}
+
+onMounted(() => {
+    emblaApi.value?.on('select', updateSelected)
+    updateSelected()
+})
+
+onUnmounted(() => {
+    emblaApi.value?.off('select', updateSelected)
+})
+
+
+
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -53,13 +95,12 @@ const breadcrumbs: BreadcrumbItem[] = [
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4  rounded-xl p-2">
-            <div class=" px-4 py-10 sm:px-6 lg:px-2 lg:py-6">
+            <div class=" px-4 py-4 sm:px-4 lg:px-2 lg:py-4">
 
                 <section class="embla overflow-hidden" ref="emblaRef">
-                    <div class="embla__container flex">
+                    <div class="embla__container flex gap-1">
                         <!-- bg-red-400 -->
-                        <div class="embla__slide min-w-0  h-full rounded-xl grid grid-cols-2 lg:grid-cols-4 gap-2">
-                            <!-- .1.. -->
+                        <!-- <div class="embla__slide min-w-0  h-full rounded-xl grid grid-cols-2 lg:grid-cols-4 gap-2">
                             <div class="bg-red-200 border border-gray-400 rounded-xl">
                                 <img src="/storage/regulamento.jpg" class="w-full " />
                             </div>
@@ -72,51 +113,81 @@ const breadcrumbs: BreadcrumbItem[] = [
                             <div class="bg-red-200 border border-gray-400 rounded-xl">
                                 <img src="/storage/relatario-final-de-auto-avaliaaao-cpa-periodo-2012.jpg" />
                             </div>
+                        </div> -->
+
+                        <div class="embla__slide min-w-0 w-full h-100 bg-blue-400 rounded-xl">
+                            <img src="/storage/hero/colors.jpg" class="w-full h-full object-cover  rounded-xl" />
                         </div>
-                        <div class="embla__slide min-w-0 w-full h-[260px] bg-blue-400 rounded-xl">.2..</div>
-                        <div class="embla__slide min-w-0 w-full h-[260px] bg-gray-400 rounded-xl">.3..</div>
+                        <div class="embla__slide min-w-0 w-full h-100 bg-gray-400 rounded-xl">
+                            <img src="/storage/hero/ocean.jpg" class="w-full h-full object-cover  rounded-xl" />
+                        </div>
+                        <div class="embla__slide min-w-0 w-full h-100 bg-gray-400 rounded-xl">
+                            <img src="/storage/hero/arindam.jpg" class="w-full h-full object-cover  rounded-xl" />
+                        </div>
                     </div>
                 </section>
 
                 <section class="flex justify-between py-4 mb-4">
                     <div class="flex gap-2">
-                        <button @click="scrollPrev" class="embla__prev text-sm cursor-pointer border rounded-lg p-2">
+                        <button @click="scrollPrev"
+                            class="embla__prev text-sm cursor-pointer border rounded-lg px-2 py-1">
                             <ChevronLeft width="16" />
                         </button>
-                        <button @click="scrollNext" class="embla__next text-sm cursor-pointer border rounded-lg p-2">
+                        <button @click="scrollNext"
+                            class="embla__next text-sm cursor-pointer border rounded-lg px-2 py-1">
                             <ChevronRight width="16" />
                         </button>
                     </div>
-                    <button @click="dashboard().url" class="text-sm border cursor-pointer rounded-lg p-2">Repositório
-                        Instituicional</button>
+
+                    <div class="flex justify-center items-center space-x-3">
+                        <button v-for="(_, index) in slides" :key="'dot-' + index" @click="scrollTo(index)"
+                            class="w-3.5 h-3.5 cursor-pointer rounded-full border-2 transition-all duration-300 ease-in-out focus:outline-none"
+                            :class="selectedIndex === index
+                                ? 'border-blue-600 bg-blue-600 scale-110 shadow-sm'
+                                : 'border-gray-400 bg-transparent hover:border-blue-400 hover:scale-105'">
+                        </button>
+                    </div>
                 </section>
 
-                <!-- embla__container flex -->
+                <div class="flex gap-2">
+                    <button @click="dashboard().url"
+                        class="flex gap-1 place-items-center text-sm border cursor-pointer rounded-lg px-2 py-1">
+                        <FolderOpen width="16" />
+                        Departamentos
+                    </button>
+                    <button @click="dashboard().url"
+                        class="flex gap-1 place-items-center text-sm border cursor-pointer rounded-lg px-2 py-1">
+                        <FolderOpen width="16" />
+                        Repositório Instituicional
+                    </button>
+                </div>
+
                 <section>
-                    <div class="py-4">
+                    <div class="pb-4">
                         <h2 class="font-bold text-lg">Departamentos</h2>
                     </div>
-                    <!-- bg-red-400 -->
-                    <div class="embla__slide min-w-0  h-full rounded-xl grid grid-cols-2 lg:grid-cols-4 gap-2">
-                        <!-- .1.. -->
-                        <div class="bg-red-200 border border-gray-400 rounded-xl">
-                            <img src="/storage/regulamento.jpg" class="w-full " />
+
+                    <div class="embla__slide min-w-0  h-full grid grid-cols-2 lg:grid-cols-4 gap-2">
+                        <div>
+                            <img src="/storage/regulamento.jpg" class="w-full border border-gray-400 rounded-xl" />
                         </div>
-                        <div class="bg-red-200 border border-gray-400 rounded-xl">
-                            <img src="/storage/relatario-final-de-auto-avaliaaao-cpa-periodo-2012.jpg" />
+                        <div>
+                            <img src="/storage/relatario-final-de-auto-avaliaaao-cpa-periodo-2012.jpg"
+                                class="border border-gray-400 rounded-xl" />
                         </div>
-                        <div class="bg-red-200 border border-gray-400 rounded-xl">
-                            <img src="/storage/regulamento.jpg" />
+                        <div>
+                            <img src="/storage/regulamento.jpg" class="border border-gray-400 rounded-xl" />
                         </div>
-                        <div class="bg-red-200 border border-gray-400 rounded-xl">
-                            <img src="/storage/relatario-final-de-auto-avaliaaao-cpa-periodo-2012.jpg" />
+                        <div>
+                            <img src="/storage/relatario-final-de-auto-avaliaaao-cpa-periodo-2012.jpg"
+                                class="border border-gray-400 rounded-xl" />
                         </div>
                     </div>
                 </section>
 
                 <section class="flex justify-between items-center pt-4">
                     <div class="text-sm text-slate-500">
-                        Showing <b>1-5</b> of 45
+                        Mostrando <b>1-4</b> de 45
                     </div>
                     <div class="flex space-x-1">
                         <button
@@ -149,6 +220,5 @@ const breadcrumbs: BreadcrumbItem[] = [
 <style scoped>
 .embla__slide {
     flex: 0 0 100%;
-    min-width: 0;
 }
 </style>
