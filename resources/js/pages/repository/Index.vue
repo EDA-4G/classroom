1a9d2406a98a1a8f9012d4c0b45617a26ee62bf4
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-08 22:30:46 +0200

    refactor: add repository index page

--- resources/js/pages/repository/Index.vue
+++ resources/js/pages/repository/Index.vue
@@ -4,6 +4,17 @@ import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 
+import {
+    Breadcrumb,
+    BreadcrumbItem as BreadcrumbItem1,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
+
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -17,8 +28,36 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Repositório" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex h-full flex-1 flex-col gap-4  rounded-xl p-2">
-            <h1>Repositório Instituicional</h1>
+        <div class="flex h-full flex-1 flex-col gap-4  rounded-xl p-2 px-4">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem1>
+                        <BreadcrumbLink href="/">
+                            Início
+                        </BreadcrumbLink>
+                    </BreadcrumbItem1>
+                    <BreadcrumbSeparator />
+
+                    <BreadcrumbItem1>
+                        <BreadcrumbPage>Repositório</BreadcrumbPage>
+                    </BreadcrumbItem1>
+                </BreadcrumbList>
+            </Breadcrumb>
+
+
+            <InputGroup>
+                <InputGroupInput id="#" placeholder="Departamento, sala..." />
+                <InputGroupAddon>
+                    <Search />
+                </InputGroupAddon>
+                <InputGroupAddon align="inline-end">
+                    12 resultados
+                </InputGroupAddon>
+            </InputGroup>
+
+            <section>
+                
+            </section>
         </div>
     </AppLayout>
 </template>
