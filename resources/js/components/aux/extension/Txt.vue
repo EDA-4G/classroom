e9623ddbdad124e61834e6b7a24c2e162c05a447
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-09 16:00:38 +0200

    feat: add txt extension icon

---
+++ resources/js/components/aux/extension/Txt.vue
@@ -0,0 +1,25 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+        <linearGradient id="sMyYmC40NDm5k2OUHmHi6a_II88qZ5egGrP_gr1" x1="-8.398" x2="84.447" y1="51.185" y2="-26.721"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#faf3df"></stop>
+            <stop offset="1" stop-color="#84b8aa"></stop>
+        </linearGradient>
+        <path fill="url(#sMyYmC40NDm5k2OUHmHi6a_II88qZ5egGrP_gr1)"
+            d="M40,16v23c0,2.761-2.239,5-5,5H8V9c0-2.761,2.239-5,5-5h15L40,16z"></path>
+        <linearGradient id="sMyYmC40NDm5k2OUHmHi6b_II88qZ5egGrP_gr2" x1="42.703" x2="27.399" y1="1.297" y2="16.601"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#faf3df" stop-opacity="0"></stop>
+            <stop offset="1" stop-color="#255b70"></stop>
+        </linearGradient>
+        <path fill="url(#sMyYmC40NDm5k2OUHmHi6b_II88qZ5egGrP_gr2)" d="M28,4v8c0,2.209,1.791,4,4,4h8L28,4z"></path>
+        <linearGradient id="sMyYmC40NDm5k2OUHmHi6c_II88qZ5egGrP_gr3" x1="6.54" x2="28.782" y1="75.97" y2="14.861"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#faf3df" stop-opacity="0"></stop>
+            <stop offset="1" stop-color="#255b70"></stop>
+        </linearGradient>
+        <path fill="url(#sMyYmC40NDm5k2OUHmHi6c_II88qZ5egGrP_gr3)"
+            d="M33,26H18c-1.657,0-3,1.343-3,3v1h15c1.657,0,3-1.343,3-3V26z M24,18h-6c-1.657,0-3,1.343-3,3	v1h6c1.657,0,3-1.343,3-3V18z M33,34H18c-1.657,0-3,1.343-3,3v1h15c1.657,0,3-1.343,3-3V34z">
+        </path>
+    </svg>
+</template>
