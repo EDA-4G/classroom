74450e004807c1faeedee11ebbafec40620ad6c7
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-09 16:02:41 +0200

    feat: add image extension icon

---
+++ resources/js/components/aux/extension/Image.vue
@@ -0,0 +1,37 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+        <linearGradient id="bQjvFlPTYB1pYe9C4wmvDa_glkcCL95mj1v_gr1" x1="-8.398" x2="74.731" y1="51.185" y2="-18.568"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#faf3df"></stop>
+            <stop offset="1" stop-color="#84b8aa"></stop>
+        </linearGradient>
+        <path fill="url(#bQjvFlPTYB1pYe9C4wmvDa_glkcCL95mj1v_gr1)"
+            d="M40,16v23c0,2.761-2.239,5-5,5H8V9c0-2.761,2.239-5,5-5h15L40,16z"></path>
+        <linearGradient id="bQjvFlPTYB1pYe9C4wmvDb_glkcCL95mj1v_gr2" x1="42.703" x2="27.399" y1="1.297" y2="16.601"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#faf3df" stop-opacity="0"></stop>
+            <stop offset="1" stop-color="#255b70"></stop>
+        </linearGradient>
+        <path fill="url(#bQjvFlPTYB1pYe9C4wmvDb_glkcCL95mj1v_gr2)" d="M28,4v8c0,2.209,1.791,4,4,4h8L28,4z"></path>
+        <linearGradient id="bQjvFlPTYB1pYe9C4wmvDc_glkcCL95mj1v_gr3" x1="18.425" x2="35.795" y1="36.491" y2="19.121"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#e8ddb4" stop-opacity="0"></stop>
+            <stop offset="1" stop-color="#8f4c5e"></stop>
+        </linearGradient>
+        <circle cx="30.958" cy="23.958" r="2.958" fill="url(#bQjvFlPTYB1pYe9C4wmvDc_glkcCL95mj1v_gr3)"></circle>
+        <linearGradient id="bQjvFlPTYB1pYe9C4wmvDd_glkcCL95mj1v_gr4" x1="40.443" x2="28.084" y1="30.293" y2="42.652"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#76bfbd" stop-opacity="0"></stop>
+            <stop offset="1" stop-color="#1b6166"></stop>
+        </linearGradient>
+        <path fill="url(#bQjvFlPTYB1pYe9C4wmvDd_glkcCL95mj1v_gr4)"
+            d="M39.5,41.17C38.7,42.85,36.98,44,35,44h-1.42l-8.59-8.59l4.54-4.22L39.5,41.17z"></path>
+        <linearGradient id="bQjvFlPTYB1pYe9C4wmvDe_glkcCL95mj1v_gr5" x1="14.664" x2="27.27" y1="56.405" y2="21.77"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#76bfbd" stop-opacity="0"></stop>
+            <stop offset="1" stop-color="#1b6166"></stop>
+        </linearGradient>
+        <polygon fill="url(#bQjvFlPTYB1pYe9C4wmvDe_glkcCL95mj1v_gr5)" points="33.583,44 8,44 8,35.14 16.583,27">
+        </polygon>
+    </svg>
+</template>
