5da3c3f914290c6ef9c6403f5b8d4d803039aea1
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-09 15:57:11 +0200

    feat: add other extension icon

---
+++ resources/js/components/aux/extension/Other.vue
@@ -0,0 +1,17 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+        <linearGradient id="-l1NVvjXZYK-cDBj9StJMa_NTG1DYcVIaS5_gr1" x1="-8.398" x2="84.447" y1="51.185" y2="-26.721"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#faf3df"></stop>
+            <stop offset="1" stop-color="#84b8aa"></stop>
+        </linearGradient>
+        <path fill="url(#-l1NVvjXZYK-cDBj9StJMa_NTG1DYcVIaS5_gr1)"
+            d="M40,16v23c0,2.761-2.239,5-5,5H8V9c0-2.761,2.239-5,5-5h15L40,16z"></path>
+        <linearGradient id="-l1NVvjXZYK-cDBj9StJMb_NTG1DYcVIaS5_gr2" x1="42.703" x2="27.399" y1="1.297" y2="16.601"
+            gradientUnits="userSpaceOnUse">
+            <stop offset="0" stop-color="#faf3df" stop-opacity="0"></stop>
+            <stop offset="1" stop-color="#255b70"></stop>
+        </linearGradient>
+        <path fill="url(#-l1NVvjXZYK-cDBj9StJMb_NTG1DYcVIaS5_gr2)" d="M28,4v8c0,2.209,1.791,4,4,4h8L28,4z"></path>
+    </svg>
+</template>
