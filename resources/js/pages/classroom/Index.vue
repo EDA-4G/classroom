57694dd02a067958ee48ee375f7b31b203c6d7dd
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-10 20:57:57 +0200

    refactor: add classroom index page

--- resources/js/pages/classroom/Index.vue
+++ resources/js/pages/classroom/Index.vue
@@ -13,6 +13,19 @@ import {
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
 
+import { BadgeCheckIcon, ChevronRightIcon, Info } from "lucide-vue-next"
+import { Button } from "@/components/ui/button"
+import {
+    Item,
+    ItemActions,
+    ItemContent,
+    ItemDescription,
+    ItemMedia,
+    ItemTitle,
+} from "@/components/ui/item"
+
+import departments from '@/routes/departments';
+
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -38,10 +51,75 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <BreadcrumbSeparator />
 
                     <BreadcrumbItem1>
-                        <BreadcrumbPage>Sala de aulas</BreadcrumbPage>
+                        <BreadcrumbLink :href="departments.index().url">
+                            Departamentos
+                        </BreadcrumbLink>
+                    </BreadcrumbItem1>
+                    <BreadcrumbSeparator />
+
+                    <BreadcrumbItem1>
+                        <BreadcrumbPage>Sala</BreadcrumbPage>
                     </BreadcrumbItem1>
                 </BreadcrumbList>
             </Breadcrumb>
+
+            <section>
+                <p class="text-lg font-semibold">Estruturas de Dados e Algoritmos</p>
+                <p class="text-sm text-gray-400">Engenharia Informatica Pos-Laboral, 2 Ano</p>
+            </section>
+
+            <section class="grid lg:grid-cols-2 gap-4">
+                <section class="grid place-items-center border border-gray-500 h-100 rounded-lg">
+                    00:00:00
+                </section>
+                <section>
+                    <div class="grid gap-2">
+                        <Item variant="outline" size="sm" as-child>
+                            <a href="#">
+                                <ItemMedia>
+                                    <!-- <BadgeCheckIcon class="size-5" /> -->
+                                    <Info class="size-5" />
+                                </ItemMedia>
+                                <ItemContent>
+                                    <ItemTitle>Director/a do departamento</ItemTitle>
+                                </ItemContent>
+                                <ItemActions>
+                                    <ChevronRightIcon class="size-4" />
+                                </ItemActions>
+                            </a>
+                        </Item>
+                        <Item variant="outline" size="sm" as-child>
+                            <a href="#">
+                                <ItemMedia>
+                                    <BadgeCheckIcon class="size-5" />
+                                    <!-- <Info class="size-5"/> -->
+                                </ItemMedia>
+                                <ItemContent>
+                                    <ItemTitle>Director/a do curso</ItemTitle>
+                                </ItemContent>
+                                <ItemActions>
+                                    <ChevronRightIcon class="size-4" />
+                                </ItemActions>
+                            </a>
+                        </Item>
+                    </div>
+
+
+                    B
+                    Directora do departamento
+                    Directora do curso
+                    Nome do docente
+                    Disciplina em aula
+                    Fixar Sala
+                    Mostrar hora de inicio/ fim
+                    Mostrar duracao da aula
+                    Repositorio da Sala(Publicar trabalhos)
+                    Estudantes acompanhando a Aula
+                    Gerar lista de presenca e estudante marcar presenca
+                    Publicar algum achado na sala
+                    <button>Voltar</button>
+                </section>
+            </section>
 
         </div>
     </AppLayout>
