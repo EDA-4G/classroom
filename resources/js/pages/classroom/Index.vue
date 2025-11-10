5ae00a1a0bd9ddc6b137362593626f6d5f0bd181
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-10 17:09:36 +0200

    feat: add classroom index page

---
+++ resources/js/pages/classroom/Index.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { dashboard } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+import {
+    Breadcrumb,
+    BreadcrumbItem as BreadcrumbItem1,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: dashboard().url,
+    },
+];
+</script>
+
+<template>
+
+    <Head title="Sala de Aulas" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4  rounded-xl p-4 px-6 lg:px-4">
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem1>
+                        <BreadcrumbLink :href="dashboard().url">
+                            Início
+                        </BreadcrumbLink>
+                    </BreadcrumbItem1>
+                    <BreadcrumbSeparator />
+
+                    <BreadcrumbItem1>
+                        <BreadcrumbPage>Sala de aulas</BreadcrumbPage>
+                    </BreadcrumbItem1>
+                </BreadcrumbList>
+            </Breadcrumb>
+
+        </div>
+    </AppLayout>
+</template>
