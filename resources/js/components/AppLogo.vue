1df0cbf4882a8f560227c3d423431036f07e358f
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-17 22:24:18 +0200

    refactor: add AppLogo component

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -3,10 +3,10 @@ import AppLogoIcon from '@/components/AppLogoIcon.vue';
 </script>
 
 <template>
-    <div>
-        <img src="/storage/logo.png" class="w-4 h-full" />
+    <div class="size-6">
+        <AppLogoIcon />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate leading-tight font-semibold">Sala</span>
+        <span class="mb-0.5 truncate leading-tight font-semibold">Salai</span>
     </div>
 </template>
