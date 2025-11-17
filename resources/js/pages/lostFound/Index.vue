a338d51ad1c89df496c138365ab3868b3b0037d9
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-18 00:14:05 +0200

    feat: add Thank You section

--- resources/js/pages/lostFound/Index.vue
+++ resources/js/pages/lostFound/Index.vue
@@ -41,6 +41,18 @@ import {
 } from '@/components/ui/select'
 import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
 
+import { Label } from "@/components/ui/label"
+import {
+    Sheet,
+    SheetClose,
+    SheetContent,
+    SheetDescription,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheet"
+
 const frameworks = [
     { value: "1", label: "1" },
     { value: "2", label: "2" },
@@ -128,7 +140,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                                 class="flex gap-2 flex-wrap justify-start">
                                 <ToggleGroupItem v-for="filter in filters" :key="filter.id" :value="filter.name"
                                     @click="me(filter.name)"
-                                    class="w-fit data-[state=on]:bg-[#04724D] data-[state=on]:text-white data-[state=on]:border-[#04724D] px-3 py-3 max-h-6 text-sm hover:bg-[#048B5F] hover:text-white transition-none border border-gray-700 hover:border-[#048B5F] text-gray-700 cursor-pointer rounded-full">
+                                    class="w-fit data-[state=on]:bg-[#04724D] data-[state=on]:text-white data-[state=on]:border-[#04724D] px-3 py-3 max-h-6 text-xs hover:bg-[#048B5F] hover:text-white transition-none border border-gray-700 hover:border-[#048B5F] text-gray-700 cursor-pointer rounded-full">
                                     {{ filter.name }}
                                 </ToggleGroupItem>
                             </ToggleGroup>
@@ -139,8 +151,8 @@ const breadcrumbs: BreadcrumbItem[] = [
 
                         <section class="relative flex items-center">
                             <select
-                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm font-medium border border-[#A1B2AD] rounded-lg pl-3 pr-8 py-1 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-[#04724D] appearance-none cursor-pointer">
-                                <option value="brazil" class="">Departamento</option>
+                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-[#A1B2AD] rounded-lg pl-3 pr-8 py-1 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-[#04724D] appearance-none cursor-pointer">
+                                <option value="brazil" class="">Todos departamentos</option>
                                 <option value="brazil">Brazil</option>
                                 <option value="bucharest">Bucharest</option>
                                 <option value="london">London</option>
@@ -155,9 +167,9 @@ const breadcrumbs: BreadcrumbItem[] = [
 
                         <section class="relative flex items-center">
                             <select
-                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm font-medium border border-[#A1B2AD] rounded-lg pl-3 pr-8 py-1 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-[#04724D] appearance-none cursor-pointer">
-                                <option value="brazil" class="">Curso</option>
-                                <option value="brazil" class="">Engenharia Mecanica</option>
+                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm border border-[#A1B2AD] rounded-lg pl-3 pr-8 py-1 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-[#04724D] appearance-none cursor-pointer">
+                                <option value="brazil" class="">Todos cursos</option>
+                                <option value="brazil" class="">Eng. Mecanica</option>
                                 <option value="bucharest">Bucharest</option>
                                 <option value="london">London</option>
                                 <option value="washington">Washington</option>
@@ -309,39 +321,102 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 
 
-                        <div
-                            class="bg-neutral-primary-soft block max-w-sm p-4 lg:p-6 border border-gray-200 hover:border-gray-300 rounded-base shadow-xs rounded-lg">
-                            <a href="#" class="w-full">
-                                <img class="rounded-base w-full h-30 lg:h-60 object-cover rounded-lg"
-                                    src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                                    alt="" width="120px" />
-                            </a>
-                            <a href="#">
-                                <h5 class="mt-4 lg:mt-6 mb-1 text-md font-semibold tracking-tight text-heading">
-                                    Streamlining
-                                    your
-                                    design process today.</h5>
-                            </a>
-                            <p class="text-xs">22 Jun 2025</p>
 
-                            <div class="grid lg:grid-cols-2 items-center">
-                                <div>
-                                    <p class="mt-4">— Maria</p>
-                                    <p class="text-sm font-medium text-green-700">
-                                        Eng. Química</p>
+                        <Sheet>
+                            <SheetTrigger as-child>
+
+
+                                <button
+                                    class="bg-neutral-primary-soft text-left cursor-pointer block max-w-sm p-4 lg:p-6 border border-gray-200 hover:border-gray-300 rounded-base shadow-xs rounded-lg">
+                                    <p href="#" class="w-full">
+                                        <img class="rounded-base w-full h-30 lg:h-60 object-cover rounded-lg"
+                                            src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                                            alt="" width="120px" />
+                                    </p>
+                                    <h5 class="mt-4 lg:mt-6 mb-1 text-md font-semibold tracking-tight text-heading">
+                                        Streamlining
+                                        your
+                                        design process today.</h5>
+                                    <p class="text-xs">22 Jun 2025</p>
+
+                                    <div class="grid lg:grid-cols-2 items-center">
+                                        <div>
+                                            <p class="mt-4">— Maria</p>
+                                            <p class="text-sm font-medium text-green-700">
+                                                Eng. Química</p>
+                                        </div>
+
+                                        <div class="h-fit mt-3 flex lg:justify-end gap-2">
+                                            <span
+                                                class="rounded-full bg-green-50 px-2.5 py-1 text-xs font-semibold text-green-600 dark:bg-background dark:border dark:border-border">Relógio</span>
+                                            <span
+                                                class="rounded-full bg-green-50 px-2.5 py-1 text-xs font-semibold text-green-600 dark:bg-background dark:border dark:border-border">Apple</span>
+                                        </div>
+                                    </div>
+                                </button>
+
+
+                            </SheetTrigger>
+                            <SheetContent>
+                                <SheetHeader>
+                                    <SheetTitle class="text-xl">Streamlining your design process today.</SheetTitle>
+                                    <SheetDescription>
+                                        Faça as alterações da sua inscrição aqui e clique em Salvar
+                                        Alterações quando terminar.
+                                    </SheetDescription>
+                                </SheetHeader>
+                                <div class="grid gap-4 p-4">
+                                    <div class="grid items-center gap-4">
+                                        <Label for="name" class="text-right">
+                                            Estudante
+                                        </Label>
+                                        <Input id="student-email" placeholder="Ex: nome.apelido@uem.ac.mz"
+                                            class="col-span-3" />
+                                    </div>
+                                    <div class="grid items-center gap-4">
+                                        <Label for="name" class="text-right">
+                                            Email
+                                        </Label>
+                                        <Input id="personal-email" placeholder="Ex: eda.grupo4@gmail.com"
+                                            class="col-span-3" />
+                                    </div>
+                                    <div class="inline-flex gap-2">
+                                        <div class="relative inline-block w-11 h-5">
+                                            <input id="switch-component-desc" type="checkbox"
+                                                class="peer appearance-none w-11 h-5 bg-slate-100 rounded-full checked:bg-[#024625] cursor-pointer transition-colors duration-300" />
+                                            <label for="switch-component-desc"
+                                                class="absolute top-0 left-0 w-5 h-5 bg-white rounded-full border border-slate-300 shadow-sm transition-transform duration-300 peer-checked:translate-x-6 peer-checked:border-slate-800 cursor-pointer">
+                                            </label>
+                                        </div>
+
+                                        <label for="switch-component-desc"
+                                            class="text-slate-600 text-sm cursor-pointer">
+                                            <div>
+                                                <p class="font-medium">
+                                                    Habilitar a Inscrição
+                                                </p>
+                                                <p class="text-slate-500">
+                                                    Poderá habilitar e desabilitar o estado da sua inscrição.
+                                                </p>
+                                            </div>
+                                        </label>
+                                    </div>
                                 </div>
+                                <SheetFooter>
+                                    <SheetClose as-child>
+                                        <button type="submit"
+                                            class="p-2 text-sm rounded-md border border-[#038043] bg-[#038043] text-white hover:bg-[#1fad68] cursor-pointer">Recuperar</button>
+                                    </SheetClose>
+                                    <SheetClose as-child>
+                                        <button type="submit"
+                                            class="p-2 text-sm rounded-md border border-[#038043]  text-[#038043] hover:text-[#1fad68] cursor-pointer">Cancelar</button>
+                                    </SheetClose>
+                                </SheetFooter>
+                            </SheetContent>
+                        </Sheet>
 
-                                <div class="h-fit mt-3 flex lg:justify-end gap-2">
-                                    <span
-                                        class="rounded-full bg-green-50 px-2.5 py-1 text-xs font-semibold text-green-600 dark:bg-background dark:border dark:border-border">Relógio</span>
-                                    <span
-                                        class="rounded-full bg-green-50 px-2.5 py-1 text-xs font-semibold text-green-600 dark:bg-background dark:border dark:border-border">Apple</span>
-                                </div>
-                            </div>
 
-                        </div>
                     </section>
-
 
                 </div>
             </section>
