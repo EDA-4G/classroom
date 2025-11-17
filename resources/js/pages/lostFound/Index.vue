12b2615a5e39bb1577943463296472b73ec44a2d
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-17 23:36:17 +0200

    refactor: add section of filters

--- resources/js/pages/lostFound/Index.vue
+++ resources/js/pages/lostFound/Index.vue
@@ -128,7 +128,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                                 class="flex gap-2 flex-wrap justify-start">
                                 <ToggleGroupItem v-for="filter in filters" :key="filter.id" :value="filter.name"
                                     @click="me(filter.name)"
-                                    class="w-fit data-[state=on]:bg-[#04724D] data-[state=on]:text-white data-[state=on]:border-[#04724D] px-3 py-3 max-h-6 text-xs hover:bg-[#048B5F] hover:text-white transition-none border border-gray-700 hover:border-[#048B5F] text-gray-700 cursor-pointer rounded-full">
+                                    class="w-fit data-[state=on]:bg-[#04724D] data-[state=on]:text-white data-[state=on]:border-[#04724D] px-3 py-3 max-h-6 text-sm hover:bg-[#048B5F] hover:text-white transition-none border border-gray-700 hover:border-[#048B5F] text-gray-700 cursor-pointer rounded-full">
                                     {{ filter.name }}
                                 </ToggleGroupItem>
                             </ToggleGroup>
@@ -136,56 +136,45 @@ const breadcrumbs: BreadcrumbItem[] = [
 
                     </div>
                     <div class="flex flex-col items-end lg:flex-row lg:justify-end gap-2">
-                        <Select>
-                            <SelectTrigger class="w-fit h-7 text-xs outline-none rounded-lg">
-                                <SelectValue placeholder="Selecionar departamento" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="apple">
-                                    Engenharia Mecanica
-                                </SelectItem>
-                                <SelectItem value="banana">
-                                    Banana
-                                </SelectItem>
-                                <SelectItem value="blueberry">
-                                    Blueberry
-                                </SelectItem>
-                                <SelectItem value="grapes">
-                                    Grapes
-                                </SelectItem>
-                                <SelectItem value="pineapple">
-                                    Pineapple
-                                </SelectItem>
-                            </SelectContent>
-                        </Select>
-                        <Select>
-                            <SelectTrigger class="w-fit h-7 text-xs outline-none rounded-lg">
-                                <SelectValue placeholder="Selecionar curso" />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="apple">
-                                    Engenharia Mecanica
-                                </SelectItem>
-                                <SelectItem value="banana">
-                                    Banana
-                                </SelectItem>
-                                <SelectItem value="blueberry">
-                                    Blueberry
-                                </SelectItem>
-                                <SelectItem value="grapes">
-                                    Grapes
-                                </SelectItem>
-                                <SelectItem value="pineapple">
-                                    Pineapple
-                                </SelectItem>
-                            </SelectContent>
-                        </Select>
+
+                        <section class="relative flex items-center">
+                            <select
+                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm font-medium border border-[#A1B2AD] rounded-lg pl-3 pr-8 py-1 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-[#04724D] appearance-none cursor-pointer">
+                                <option value="brazil" class="">Departamento</option>
+                                <option value="brazil">Brazil</option>
+                                <option value="bucharest">Bucharest</option>
+                                <option value="london">London</option>
+                                <option value="washington">Washington</option>
+                            </select>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2"
+                                stroke="currentColor" class="h-4 w-4 ml-1 absolute top-2 right-2.5 text-slate-700">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                            </svg>
+                        </section>
+
+                        <section class="relative flex items-center">
+                            <select
+                                class="w-full bg-transparent placeholder:text-slate-400 text-slate-700 text-sm font-medium border border-[#A1B2AD] rounded-lg pl-3 pr-8 py-1 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-[#04724D] appearance-none cursor-pointer">
+                                <option value="brazil" class="">Curso</option>
+                                <option value="brazil" class="">Engenharia Mecanica</option>
+                                <option value="bucharest">Bucharest</option>
+                                <option value="london">London</option>
+                                <option value="washington">Washington</option>
+                            </select>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2"
+                                stroke="currentColor" class="h-4 w-4 ml-1 absolute top-2 right-2.5 text-slate-700">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                            </svg>
+                        </section>
+
                     </div>
                 </section>
 
                 <div class="py-4">
                     <div class="pb-2">
-                        <h2 class="font-semibold">Encontre o teu bem perdido.</h2>
+                        <h2 class="font-semibold text-lg text-heading">Encontre o teu bem perdido.</h2>
                     </div>
 
                     <section class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
