c092b5a059adb46ef10c2cb1439f164fd744ad8e
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-18 01:36:17 +0200

    feat: add radio-group component

--- resources/js/pages/lostFound/Index.vue
+++ resources/js/pages/lostFound/Index.vue
@@ -53,6 +53,20 @@ import {
     SheetTrigger,
 } from "@/components/ui/sheet"
 
+import {
+    Field,
+    FieldContent,
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+    FieldSet,
+    FieldTitle,
+} from '@/components/ui/field'
+import {
+    RadioGroup,
+    RadioGroupItem,
+} from '@/components/ui/radio-group'
+
 const frameworks = [
     { value: "1", label: "1" },
     { value: "2", label: "2" },
@@ -400,6 +414,43 @@ const breadcrumbs: BreadcrumbItem[] = [
                                                 </p>
                                             </div>
                                         </label>
+                                    </div>
+
+                                    <div class="w-full max-w-md">
+                                        <FieldGroup>
+                                            <FieldSet>
+                                                <FieldLabel for="compute-environment-p8w">
+                                                    Compute Environment
+                                                </FieldLabel>
+                                                <FieldDescription>
+                                                    Select the compute environment for your cluster.
+                                                </FieldDescription>
+                                                <RadioGroup default-value="kubernetes">
+                                                    <FieldLabel for="kubernetes-r2h">
+                                                        <Field orientation="horizontal">
+                                                            <FieldContent>
+                                                                <FieldTitle>Kubernetes</FieldTitle>
+                                                                <FieldDescription>
+                                                                    Run GPU workloads on a K8s configured cluster.
+                                                                </FieldDescription>
+                                                            </FieldContent>
+                                                            <RadioGroupItem id="kubernetes-r2h" value="kubernetes" />
+                                                        </Field>
+                                                    </FieldLabel>
+                                                    <FieldLabel for="vm-z4k">
+                                                        <Field orientation="horizontal">
+                                                            <FieldContent>
+                                                                <FieldTitle>Virtual Machine</FieldTitle>
+                                                                <FieldDescription>
+                                                                    Access a VM configured cluster to run GPU workloads.
+                                                                </FieldDescription>
+                                                            </FieldContent>
+                                                            <RadioGroupItem id="vm-z4k" value="vm" />
+                                                        </Field>
+                                                    </FieldLabel>
+                                                </RadioGroup>
+                                            </FieldSet>
+                                        </FieldGroup>
                                     </div>
                                 </div>
                                 <SheetFooter>
