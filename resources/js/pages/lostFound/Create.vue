12869921b2c2513e94d70d33d4a7f3d7bfa301f0
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-19 15:35:41 +0200

    refactor: add a page for posting lost items

--- resources/js/pages/lostFound/Create.vue
+++ resources/js/pages/lostFound/Create.vue
@@ -2,51 +2,18 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
-import { Head, Link } from '@inertiajs/vue3';
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import { Head } from '@inertiajs/vue3';
 import { Input } from '@/components/ui/input';
-
-
 import {
     Breadcrumb,
-    BreadcrumbEllipsis,
     BreadcrumbItem as BreadcrumbItem1,
     BreadcrumbLink,
     BreadcrumbList,
     BreadcrumbPage,
     BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Spinner } from '@/components/ui/spinner';
-import { Loader } from 'lucide-vue-next';
-import { Button } from '@/components/ui/button';
-import { Waypoints, Layers, ChevronLeft, ChevronRight, GraduationCap } from 'lucide-vue-next';
-
-import { InputGroup, InputGroupAddon, InputGroupButton, InputGroupInput, InputGroupText, InputGroupTextarea } from "@/components/ui/input-group";
-
-import { Check, ChevronsUpDown, Search } from "lucide-vue-next"
-import { ref } from "vue"
-
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group'
-
 import { Label } from "@/components/ui/label"
-import {
-    Sheet,
-    SheetClose,
-    SheetContent,
-    SheetDescription,
-    SheetFooter,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger,
-} from "@/components/ui/sheet"
-
 import {
     Field,
     FieldContent,
@@ -61,57 +28,7 @@ import {
     RadioGroupItem,
 } from '@/components/ui/radio-group'
 
-const frameworks = [
-    { value: "1", label: "1" },
-    { value: "2", label: "2" },
-    { value: "3", label: "3" },
-    { value: "4", label: "4" },
-    { value: "5", label: "5" },
-    { value: "6", label: "6" },
-
-
-]
-
-interface Filter {
-    id: number;
-    name: string
-}
-
-const filters: Filter[] = [
-    { id: 1, name: 'Todas' },
-    { id: 2, name: 'Laboral' },
-    { id: 3, name: 'Pós-Laboral' }
-]
-
-const me = (item: string) => {
-    // alert(item);
-}
-
-const filter = ref(filters ? filters.at(0)?.name : '')
-const handleToggleGroup = (value: any) => {
-    if (value) filter.value = value
-}
-
-const value = ref<typeof frameworks[0]>()
-
-
-
-
-// import { Button } from '@/components/ui/button'
-import {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-} from '@/components/ui/dialog'
-// import { Input } from '@/components/ui/input'
-// import { Label } from '@/components/ui/label'
-
-
+import classrooms from '@/routes/classrooms';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -123,7 +40,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 <template>
 
-    <Head title="Departamentos" />
+    <Head title="Publicação" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 px-6 flex-col gap-4  rounded-xl p-4 md:px-6 lg:px-4">
@@ -138,7 +55,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <BreadcrumbSeparator />
 
                     <BreadcrumbItem1>
-                        <BreadcrumbLink :href="dashboard().url">
+                        <BreadcrumbLink :href="classrooms.index().url">
                             Sala
                         </BreadcrumbLink>
                     </BreadcrumbItem1>
@@ -152,61 +69,33 @@ const breadcrumbs: BreadcrumbItem[] = [
 
             <section class="grid gap-4">
 
-                <!-- <Dialog>
-                    <form>
-                        <DialogTrigger as-child>
-                            <Button variant="outline">
-                                Open Dialog
-                            </Button>
-                        </DialogTrigger>
-                        <DialogContent class="sm:max-w-[425px]">
-                            <DialogHeader>
-                                <DialogTitle>Edit profile</DialogTitle>
-                                <DialogDescription>
-                                    Make changes to your profile here. Click save when you're
-                                    done.
-                                </DialogDescription>
-                            </DialogHeader>
-                            <div class="grid gap-4">
-                                <div class="grid gap-3">
-                                    <Label for="name-1">Name</Label>
-                                    <Input id="name-1" name="name" default-value="Pedro Duarte" />
-                                </div>
-                                <div class="grid gap-3">
-                                    <Label for="username-1">Username</Label>
-                                    <Input id="username-1" name="username" default-value="@peduarte" />
-                                </div>
-                            </div>
-                            <DialogFooter>
-                                <DialogClose as-child>
-                                    <Button variant="outline">
-                                        Cancel
-                                    </Button>
-                                </DialogClose>
-                                <Button type="submit">
-                                    Save changes
-                                </Button>
-                            </DialogFooter>
-                        </DialogContent>
-                    </form>
-                </Dialog> -->
-
-
                 <div class="">
-                    <p class="text-lg font-semibold">Postagem</p>
+                    <p class="text-lg font-semibold">Publicação</p>
                 </div>
                 <div class="grid gap-2">
-                    <Label for="email">Titulo</Label>
-                    <Input id="email" type="text" name="email" required autofocus :tabindex="1" autocomplete="email"
-                        placeholder="email@example.com" />
+                    <Label for="title">Título</Label>
+                    <Input id="title" type="text" name="title" autocomplete="title"
+                        placeholder="Informe um titúlo descritivo e claro" />
                     <InputError message="" />
                 </div>
-                <div class="grid gap-2">
-                    <Label for="email">Imagem</Label>
-                    <Input id="email" type="file" name="email" required autofocus :tabindex="1" autocomplete="email"
-                        placeholder="email@example.com" />
-                    <InputError message="" />
-                </div>
+
+                <section class="grid lg:grid-cols-3 gap-2">
+                    <div class="grid gap-2">
+                        <Label for="email">Imagem</Label>
+                        <Input id="email" type="file" name="email" />
+                        <InputError message="" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="type">Tipo do bem</Label>
+                        <Input id="type" type="text" name="type" autocomplete="type" placeholder="Ex: Caderno" />
+                        <InputError message="" />
+                    </div>
+                    <div class="grid gap-2">
+                        <Label for="color">Cor</Label>
+                        <Input id="color" type="text" name="color" autocomplete="color" placeholder="Ex: Preto" />
+                        <InputError message="" />
+                    </div>
+                </section>
 
                 <div>
                     <FieldGroup>
@@ -258,53 +147,35 @@ const breadcrumbs: BreadcrumbItem[] = [
 
                 <section class="grid lg:grid-cols-3 gap-2">
                     <div class="grid gap-2">
-                        <Label for="email">Código</Label>
-                        <Input id="email" type="text" name="email" required autofocus :tabindex="1" autocomplete="email"
-                            placeholder="email@example.com" />
+                        <Label for="id">Código</Label>
+                        <Input id="id" type="text" name="id" placeholder="Informe o código instituicional" />
                         <InputError message="" />
                     </div>
                     <div class="grid gap-2">
                         <Label for="email">Email Instituicional</Label>
-                        <Input id="email" type="text" name="email" required autofocus :tabindex="1" autocomplete="email"
-                            placeholder="email@example.com" />
+                        <Input id="email" type="text" name="email" placeholder="Ex: nome.apelido@uem.ac.mz" />
                         <InputError message="" />
                     </div>
                 </section>
 
                 <div class="grid gap-2">
                     <Label for="email">Onde encontrou o pertence</Label>
-                    <!-- <Input id="email" type="text" name="email" required autofocus :tabindex="1" autocomplete="email"
-                        placeholder="email@example.com" /> -->
                     <Select>
                         <SelectTrigger class="w-auto">
                             <SelectValue placeholder="Selecionar o departamento" />
                         </SelectTrigger>
                         <SelectContent>
-                            <!-- <SelectGroup> -->
                             <SelectItem value="apple">
                                 Cadeiras Gerais
                             </SelectItem>
                             <SelectItem value="banana">
                                 Eng. Quimica
                             </SelectItem>
-                            <!-- </SelectGroup> -->
                         </SelectContent>
                     </Select>
 
                     <InputError message="" />
                 </div>
-                <!-- <div class="grid gap-2">
-                    <Label for="email">Quem esta a publicar Docente, estudante, funcionario</Label>
-                    <Input id="email" type="text" name="email" required autofocus :tabindex="1" autocomplete="email"
-                        placeholder="email@example.com" />
-                    <InputError message="" />
-                </div> -->
-                <!-- <div class="grid gap-2">
-                    <Label for="email">Pagar/ Gratis</Label> Mpesa
-                    <Input id="email" type="text" name="email" required autofocus :tabindex="1" autocomplete="email"
-                        placeholder="email@example.com" />
-                    <InputError message="" />
-                </div> -->
 
                 <div class="inline-flex gap-2">
                     <div class="relative inline-block w-11 h-5">
@@ -318,11 +189,10 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <label for="switch-component-desc" class="text-green-900 text-sm cursor-pointer">
                         <div>
                             <p class="font-medium">
-                                Aceitar pagamento
+                                Solicitar Pagamento
                             </p>
                             <p class="text-slate-500">
-                                <!-- Poderá habilitar e desabilitar o estado da sua inscrição. -->
-                                Forma de agradecimento.
+                                Receber pagamento para entrega do bem.
                             </p>
                         </div>
                     </label>
@@ -330,7 +200,7 @@ const breadcrumbs: BreadcrumbItem[] = [
 
                 <section class="grid lg:grid-cols-3 gap-2">
                     <div class="grid items-center gap-2">
-                        <Label for="name" class="text-right">
+                        <Label for="phone" class="text-right">
                             Conta M-Pesa
                         </Label>
                         <div class="relative">
@@ -342,7 +212,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                                         clip-rule="evenodd" />
                                 </svg>
                             </span>
-                            <Input id="contactNumber" type="number"
+                            <Input id="phone" type="number" name="phone"
                                 class="w-full bg-transparent placeholder:text-gray-400 text-slate-700 text-sm border border-slate-200 rounded-md pr-3 pl-10 py-2 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
                                 placeholder="Ex: 84/85 xxx xxxx" min="1"
                                 title="Número de conta M-Pesa deve ser válido: 84/85 xxx xxxx" />
@@ -357,7 +227,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                     </div>
                 </section>
 
-                <div class="py-1 flex flex-col gap-2 lg:flex-row lg:justify-end">
+                <div class="py-4 flex flex-col gap-2 lg:flex-row lg:justify-end">
                     <button type="submit"
                         class="p-2 text-sm px-4 font-medium rounded-md border border-[#038043] bg-[#038043] text-white hover:bg-[#1fad68] hover:border-[#1fad68] cursor-pointer">
                         Publicar
