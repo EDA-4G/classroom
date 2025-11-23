5d1900b7cabd056d272d634b2576eb7ae7e349a5
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-23 22:01:11 +0200

    feat: add IClassroom interface

--- resources/js/interfaces.ts
+++ resources/js/interfaces.ts
@@ -12,3 +12,14 @@ export interface IDepartment {
     is_active: boolean;
     created_at?: Date;
 }
+
+export interface IClassroom {
+    id: number;
+    description: string;
+    image: string;
+    level: string;
+    status: string;
+    is_fixed: boolean;
+    is_active: boolean;
+    created_at?: Date;
+}
