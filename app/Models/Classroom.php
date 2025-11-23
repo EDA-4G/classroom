f7b61198f663c54e7a951409d6763431a5139454
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-23 21:34:25 +0200

    refactor: add classroom model

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Classroom extends Model
 {
-    protected $fillable = ['id', 'description', 'image', 'level', 'status', 'is_active', 'created_at'];
+    protected $fillable = ['id', 'description', 'image', 'level', 'status', 'is_fixed', 'is_active', 'created_at'];
     public function department()
     {
         return $this->belongsTo(Department::class);
