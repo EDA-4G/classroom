68f172b57304cdc74a2d0fcc52e9cd7fb50ec5c0
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-23 20:52:46 +0200

    refactor: add Department model

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Department extends Model
 {
     protected $fillable = ['id', 'description', 'is_active', 'created_at'];
+    public function classrooms()
+    {
+        return $this->hasMany(Classroom::class);
+    }
 }
