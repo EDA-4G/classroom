19b8e9e6e9516cbe7918bf41e65c0c12b3b50d9e
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-24 21:32:37 +0200

    feat: add a relationship between the user and the post

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $fillable = ['id', 'title', 'image', 'type', 'color', 'can_pay', 'is_active', 'created_at'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
