2694c684756e053883bc99270d9a47dcabf2c1eb
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-24 11:56:41 +0200

    fix: i had put the return of documents from the repository to classrooms

--- app/Http/Controllers/Admin/ClassroomController.php
+++ app/Http/Controllers/Admin/ClassroomController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Classroom;
 use App\Models\Department;
-use App\Models\RepoDocument;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -17,8 +16,9 @@ class ClassroomController extends Controller
     public function index(Request $request)
     {
         $description = $request->query('description');
-        $docs = RepoDocument::where('description', 'like', '%' . $description . '%')->paginate(5);
-        return Inertia::render('admin/classroom/Index', compact('docs'));
+        $rooms = Classroom::where('description', 'like', '%' . $description . '%')->paginate(5);
+        $deps = Department::where('is_active', true)->get();
+        return Inertia::render('admin/classroom/Index', compact('rooms', 'deps'));
     }
 
     /**
