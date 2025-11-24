02cb0a4703ff15266b4ba62f065194a85b257342
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-24 11:12:37 +0200

    refactor: add User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,11 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function documents()
+    {
+        return $this->hasMany(RepoDocument::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
