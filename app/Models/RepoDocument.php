4c8c0ddd84092af93c09c3e08d8059246f799c1c
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-24 11:11:28 +0200

    refactor: add RepDocument model

--- app/Models/RepoDocument.php
+++ app/Models/RepoDocument.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class RepoDocument extends Model
 {
     protected $fillable = ['id', 'description', 'document', 'extension', 'is_active', 'created_at'];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
