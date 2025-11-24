dea10ef0b96d59ef00a8f0f677c2e7a5ce224ef2
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-24 19:02:50 +0200

    add Post model

---
+++ app/Models/Post.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = ['id', 'title', 'image', 'type', 'color', 'can_pay', 'is_active', 'created_at'];
+}
