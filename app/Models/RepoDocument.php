48da20572d8814a52f5ae5c2a388b0a8dc781e47
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-24 10:37:18 +0200

    add RepDocument model

---
+++ app/Models/RepoDocument.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RepoDocument extends Model
+{
+    protected $fillable = ['id', 'description', 'document', 'is_active', 'created_at'];
+}
