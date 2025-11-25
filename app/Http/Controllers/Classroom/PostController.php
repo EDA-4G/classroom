2ad457d51709d124d5386f8205a211e0b4867f60
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-25 02:22:11 +0200

    refactor: add the implementation of the index method

--- app/Http/Controllers/Classroom/PostController.php
+++ app/Http/Controllers/Classroom/PostController.php
@@ -20,7 +20,7 @@ class PostController extends Controller
 
 
         $title = $request->query('title');
-        $posts = Post::where('title', 'like', '%' . $title . '%')->with(['user', 'department'])->paginate(8);
+        $posts = Post::where('title', 'like', '%' . $title . '%')->with(['user', 'department'])->orderBy('created_at', 'desc')->paginate(8);
         $deps = Department::where('is_active', true)->get();
         return Inertia::render('lostFound/Index', compact('posts', 'deps'));
     }
