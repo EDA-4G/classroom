4c8aaea8ed706704543201f836cb286e929e2259
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-25 12:36:29 +0200

    refactor: add lostFound index page

--- resources/js/pages/lostFound/Index.vue
+++ resources/js/pages/lostFound/Index.vue
@@ -183,8 +183,8 @@ const breadcrumbs: BreadcrumbItem[] = [
             </Breadcrumb>
 
             <InputGroup>
-                <InputGroupInput id="#" v-model="title" @keyup.enter="search"
-                    placeholder="Descrição, departamento, sala..." class="text-sm" />
+                <InputGroupInput id="#" v-model="title" @keyup.enter="search" placeholder="Descrição, sala..."
+                    class="text-sm" />
                 <InputGroupAddon>
                     <Search />
                 </InputGroupAddon>
@@ -313,7 +313,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                                         :src="'/storage/'.concat(post.image)" :alt="post.title" width="120px" />
                                     <div>
                                         <h5 class="mb-1 text-lg font-semibold tracking-tight text-heading">{{ post.title
-                                            }}</h5>
+                                        }}</h5>
                                         <p class="text-xs">{{ date_long_format(post.created_at) }}</p>
                                     </div>
 
@@ -321,7 +321,7 @@ const breadcrumbs: BreadcrumbItem[] = [
                                         <div>
                                             <p class="mt-0">— {{ post.user.name.split(' ').at(0) }}</p>
                                             <p class="text-sm font-medium text-green-700">{{ post.department.description
-                                                }}</p>
+                                            }}</p>
                                         </div>
 
                                         <div class="h-fit mt-2 flex flex-wrap justify-end gap-1">
