ff6f2eeaa763c1bf4242f8cf5806eb2839c35fa0
dalinezequiel <dalinezequiel44@gmail.com>
2025-11-25 11:14:25 +0200

    refactor: add the implementation of the store method

--- app/Http/Controllers/Classroom/PostController.php
+++ app/Http/Controllers/Classroom/PostController.php
@@ -37,7 +37,7 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, int $from_page)
     {
         $image_path = null;
         if ($request->hasFile('image')) {
@@ -61,7 +61,10 @@ class PostController extends Controller
         $post->department()->associate($department);
 
         $post->save();
-        return redirect()->route('classrooms.index')->with('success', 'Cadastrado com sucesso!');
+
+        if ($from_page === 1)
+            return redirect()->route('classrooms.index')->with('success', 'Cadastrado com sucesso!');
+        return redirect()->route('posts.index')->with('success', 'Cadastrado com sucesso!');
     }
 
     /**
